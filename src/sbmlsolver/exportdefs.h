85a1890397ada7b2adf694b58b91fed088593476
raimc <raimc>
2005-06-28 14:08:45 +0000

    replace C++ style comments

--- src/sbmlsolver/exportdefs.h
+++ src/sbmlsolver/exportdefs.h
@@ -1,9 +1,10 @@
-// The following ifdef block is the standard way of creating macros which make exporting 
-// from a DLL simpler. All files within this DLL are compiled with the SBML_ODESOLVER_EXPORTS
-// symbol defined on the command line. this symbol should not be defined on any project
-// that uses this DLL. This way any other project whose source files include this file see 
-// SBML_ODESOLVER_API functions as being imported from a DLL, whereas this DLL sees symbols
-// defined with this macro as being exported.
+/* The following ifdef block is the standard way of creating macros
+which make exporting from a DLL simpler. All files within this DLL are
+compiled with the SBML_ODESOLVER_EXPORTS symbol defined on the command
+line. this symbol should not be defined on any project that uses this
+DLL. This way any other project whose source files include this file
+see SBML_ODESOLVER_API functions as being imported from a DLL, whereas
+this DLL sees symbols defined with this macro as being exported. */
 #ifdef WIN32
 #ifdef SBML_ODESOLVER_EXPORTS
 #define SBML_ODESOLVER_API __declspec(dllexport)
