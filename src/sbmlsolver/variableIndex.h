84ee1345f451bd60c97983349ad5b9b6f0183d69
afinney <afinney>
2005-07-26 15:42:50 +0000

    removed type field in preparation for RM's simplification and optimization

--- src/sbmlsolver/variableIndex.h
+++ src/sbmlsolver/variableIndex.h
@@ -1,16 +1,8 @@
 #ifndef _VARIABLEINDEX_H_
 #define _VARIABLEINDEX_H_
 
-typedef enum variableType
-{
-    SPECIES,
-    ASSIGNMENT_PARAMETER,
-    PARAMETER
-} variableType_t;
-
 struct variableIndex
 {
-    variableType_t type ;
     int index ;
 } ;
 
