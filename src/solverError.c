98d695beec4d8b61c71c12ba2c9930b738c72109
raimc <raimc>
2005-10-26 15:32:12 +0000

    added libSBML-like licencse declarations

--- src/solverError.c
+++ src/solverError.c
@@ -1,7 +1,36 @@
 /*
-  Last changed Time-stamp: <2005-10-26 16:57:32 raim>
-  $Id: solverError.c,v 1.9 2005/10/26 14:57:39 raimc Exp $ 
+  Last changed Time-stamp: <2005-10-26 17:12:21 raim>
+  $Id: solverError.c,v 1.10 2005/10/26 15:32:12 raimc Exp $ 
 */
+/* 
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. The software and
+ * documentation provided hereunder is on an "as is" basis, and the
+ * authors have no obligations to provide maintenance, support,
+ * updates, enhancements or modifications.  In no event shall the
+ * authors be liable to any party for direct, indirect, special,
+ * incidental or consequential damages, including lost profits, arising
+ * out of the use of this software and its documentation, even if the
+ * authors have been advised of the possibility of such damage.  See
+ * the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ *
+ * The original code contained here was initially developed by:
+ *
+ *     Andrew M. Finney
+ *
+ * Contributor(s):
+ */
 
 #include "sbmlsolver/solverError.h"
 
