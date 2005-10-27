54a724ef77f5a19143f9f4a30b5aa07804ce50a2
afinney <afinney>
2005-10-27 13:09:34 +0000

    replace CVODE with sundials

---
+++ Win32/WinCVODE/sundials/config/sundials_config.h
@@ -0,0 +1,33 @@
+/*
+ * -----------------------------------------------------------------
+ * $Revision: 1.1 $
+ * $Date: 2005/10/27 13:11:25 $
+ * ----------------------------------------------------------------- 
+ * Programmer(s): Aaron Collier @ LLNL
+ * -----------------------------------------------------------------
+ * Copyright (c) 2005, The Regents of the University of California.
+ * Produced at the Lawrence Livermore National Laboratory.
+ * All rights reserved.
+ * For details, see sundials/shared/LICENSE.
+ *------------------------------------------------------------------
+ * SUNDIALS configuration header file
+ *------------------------------------------------------------------
+ */
+
+/* Define SUNDIALS version number */
+#define SUNDIALS_PACKAGE_VERSION "2.1.1"
+
+/* FCMIX: Define Fortran name-mangling macro */
+
+
+/* FCMIX: Define case of function names */
+
+
+/* FCMIX: Define number of underscores to append to function names */
+
+
+/* Define precision of SUNDIALS data type 'realtype' */
+#define SUNDIALS_DOUBLE_PRECISION 1
+
+/* Use generic math functions */
+#define SUNDIALS_USE_GENERIC_MATH 1
