9b4332378de3a50592c829f9d695dc4d0b56ecf9
afinney <afinney>
2005-12-07 17:40:54 +0000

    removeing this

--- Win32/makerelease.bat
+++
@@ -1,6 +0,0 @@
-copy binary\*.* release
-copy bin\SBML_odeSolver.dll release
-copy bin\Sense.exe release
-copy bin\WinCVODE.dll release
-copy bin\SBML_odeSolverApp.exe release
-copy bin\ParameterScanner.exe release
